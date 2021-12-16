efb34091169f2d30abd831946fde54679655a12e
Henrique Marques <hfvmarques@gmail.com>
2021-12-15 20:09:24 -0400

    relacionais

---
+++ Modulo_02/04_relacionais.rb
@@ -0,0 +1,71 @@
+if 1 > 2
+  puts 'a'
+else
+  puts 'b'
+end
+
+if 1 >= 2
+  puts 'a'
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 'a'
+else
+  puts 'b'
+end
+
+if 1 <= 2
+  puts 'a'
+else
+  puts 'b'
+end
+
+if 1 == 2
+  puts 'a'
+else
+  puts 'b'
+end
+
+if 1 != 2
+  puts 'a'
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 1+1
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 1-1
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 3*2
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 10/2
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 10**2
+else
+  puts 'b'
+end
+
+if 1 < 2
+  puts 10%2
+else
+  puts 'b'
+end
