80eb64ad4f24c5b191bba124396a585891b89cc2
Henrique Marques <hfvmarques@gmail.com>
2021-12-16 11:11:48 -0400

    symbols

---
+++ Modulo_02/09_symbols.rb
@@ -0,0 +1,20 @@
+puts "Henrique".object_id
+puts "Henrique".object_id
+puts "Henrique".object_id
+puts "Henrique".object_id
+puts "Henrique".object_id
+puts "=================="
+
+puts :henrique.object_id
+puts :henrique.object_id
+puts :henrique.object_id
+puts :henrique.object_id
+puts :henrique.object_id
+
+j = { "nome": "Henrique"}
+puts j.object_id
+puts j.object_id
+
+h = { curso: "Rails"}
+puts h.object_id
+puts h.object_id
